6804625a6ca9631778fe29b36cd5b2c0e024f469
Barry Hanna <barryahanna@gmail.com>
2024-04-17 09:33:11 +0100

    report function

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(num_of_words(file_contents))
-        print_letter_occurances(letter_occurances(file_contents))
+        words = num_of_words(file_contents)
+        letters = letter_occurances(file_contents)
+        total = sorted(letters.items(), key=lambda item: item[1], reverse=True)
+        print_book_report("books/frankenstein.txt", words, total)
 
 
 def num_of_words(str):
@@ -12,12 +14,22 @@ def num_of_words(str):
 def letter_occurances(str):
     count = {}
     for char in str:
+        if not char.isalpha():
+            continue
         lower_char = char.lower()
         if lower_char in count:
             count[lower_char] += 1
         else:
             count[lower_char] = 1
     return count
+
+
+def print_book_report(file, words, character_total):
+    print(f"--- Being report of {file} ---")
+    print(f"{words} found in the document\n")
+    for char, total in character_total:
+        print(f"The '{char}' was found {total} times")
+    print("--- End report ---")
 
 
 def print_letter_occurances(count):
